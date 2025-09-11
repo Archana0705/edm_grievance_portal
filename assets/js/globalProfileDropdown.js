770608b13388371a9284b1625f3dcf03752c79a2
Archana0705 <archanathoshik@gmail.com>
2025-09-11 14:42:18 +0530

    profile-login-register-api-integration-added

--- assets/js/globalProfileDropdown.js
+++ assets/js/globalProfileDropdown.js
@@ -2,7 +2,8 @@ $(function () {
     // Load profile dropdown into placeholder
     $("#globalProfileDropdown").load("assets/partials/profileDropdown.html", function () {
         // Update username dynamically if stored
-        const username = localStorage.getItem("userName") || "User";
+        const username = localStorage.getItem("user_name") || "User";
+        const userid = localStorage.getItem("user_id") || "";
         $("#userName").text(username);
 
         // Attach sign out functionality
@@ -10,10 +11,11 @@ $(function () {
             e.preventDefault();
 
             // Clear all possible session storage
-            // localStorage.removeItem("authToken");
-            // localStorage.removeItem("userName");
-            // sessionStorage.clear();
-            // document.cookie = "sessionId=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;";
+            //localStorage.removeItem("authToken");
+            localStorage.removeItem("user_name");
+            localStorage.removeItem("user_id");
+            sessionStorage.clear();
+            document.cookie = "sessionId=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;";
 
             // Redirect to login page
             window.location.href = "index.html";
