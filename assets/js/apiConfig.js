fcd72e5828cfef4d5afa6f98ce2b58ec8645d3b3
Archana0705 <archanathoshik@gmail.com>
2025-09-22 12:58:49 +0530

    latest

--- assets/js/apiConfig.js
+++ assets/js/apiConfig.js
@@ -1,4 +1,15 @@
-const environment = "prod";
+// const environment = "dev";
+
+// const baseUrls = {
+//     // dev: "http://10.236.210.57/tnapex_api",
+//     dev: "https://tngis.tnega.org/lcap_api/edm_grievance_portal/v1",
+//     prod: "https://tngis.tnega.org/lcap_api/edm_grievance_portal/v1"
+// };
+
+// window.BASE_API_URL = baseUrls[environment];
+// window.BASE_UPLOAD_URL = window.BASE_API_URL.replace(/\/v1$/, '') + "/uploads/";
+
+const environment = "dev";
 
 const baseUrls = {
     // dev: "http://10.236.210.57/tnapex_api",
@@ -7,4 +18,4 @@ const baseUrls = {
 };
 
 window.BASE_API_URL = baseUrls[environment];
-window.BASE_UPLOAD_URL = window.BASE_API_URL.replace(/\/v1$/, '') + "/uploads/";
+window.BASE_UPLOAD_URL = "https://tngis.tnega.org/edm_grievance_portal/uploads/";
